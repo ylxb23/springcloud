f9c8c9c74cc1b84bfbdabd874b27aa823b0ac837
ylxb23 <ylxb23@163.com>
2017-11-10 16:50:50 +0800

    通过访问http://localhost:3051/bus/refresh发送消息通知configserver重新读取git仓库中的配置(需消息队列支持如rabbitMQ)

--- configclient/src/main/java/com/zero/spring/cloud/config/init/ConfigClientApp.java
+++ configclient/src/main/java/com/zero/spring/cloud/config/init/ConfigClientApp.java
@@ -10,7 +10,7 @@ import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
  * @date 2017年11月10日 上午10:54:06
  * @author zero
  */
-@RefreshScope
+@RefreshScope	// 通过访问 http://localhost:3051/bus/refresh 发送消息通知 configserver重新读取 git仓库中的配置(需消息队列支持如rabbitMQ)
 @EnableEurekaClient
 @SpringBootApplication(scanBasePackages= {"com.zero.spring.cloud.config"})
 public class ConfigClientApp {
